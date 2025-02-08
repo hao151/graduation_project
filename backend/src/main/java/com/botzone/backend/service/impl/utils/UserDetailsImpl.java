f87d561c80202b466c6442d4ce517d93997106ab
heeeeee <313347408@qq.com>
2025-02-08 17:43:12 +0800

    security details

--- backend/src/main/java/com/botzone/backend/service/impl/utils/UserDetailsImpl.java
+++ backend/src/main/java/com/botzone/backend/service/impl/utils/UserDetailsImpl.java
@@ -3,7 +3,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import com.botzone.backend.pojo.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
