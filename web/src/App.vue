9d5b2dcc2bdf8568e45ad817ffb4660978b35718
heeeeee <313347408@qq.com>
2025-05-21 15:12:19 +0800

    background

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,7 +9,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
 import NavBar from './components/NavBar.vue'
-//import $ from 'jquery'
 
 export default{
   components: {
@@ -17,51 +16,7 @@ export default{
   },
 
   setup(){
-    // $.ajax({
-    //   url:"http://127.0.0.1:3000/user/account/token/",
-    //   type: "post",
-    //   data:{
-    //     username:'hehao',
-    //     password:'123',
-    //   },
-    //   success(resp){
-    //     console.log(resp);
-    //   },
-    //   error(resp){
-    //     console.log(resp);
-    //   } 
-    // });
-
-    // $.ajax({
-    //   url:"http://127.0.0.1:3000/user/account/info/",
-    //   type: "get",
-    //   headers:{
-    //     Authorization:"Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJkZWRmZGJhZGVmZDM0YmM4OGRiY2RmZjhiYWQwNTYwNCIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTczOTA3NzIyMSwiZXhwIjoxNzQwMjg2ODIxfQ.0uQ1e_PjCKNFzLpnw3ruARbMJUwD1ZiCd-1KQ3Nw7Ys"
-    //   },
-    //   success(resp){
-    //     console.log(resp);
-    //   },
-    //   error(resp){
-    //     console.log(resp);
-    //   }
-    // });
-
-    // $.ajax({
-    //   url:"http://127.0.0.1:3000/user/account/register/",
-    //   type:"post",
-    //   data:{
-    //     username:'hehe',
-    //     password:"123",
-    //     confirmPassword:"123",
-    //   },
-    //   success(resp){
-    //     console.log(resp);
-    //   },
-    //   error(resp){
-    //     console.log(resp);
-    //   }
-    // })
-
+   
 
   }
    
@@ -71,8 +26,11 @@ export default{
 </script>
 
 
-
 <style>
+body{
+  background-image: url("@/assets/img/background.png");
+
+}
 
 </style>
 
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -3,6 +3,8 @@
         <PlayGround v-if="$store.state.pk.status === 'playing'"></PlayGround>
         <MatchGround v-if="$store.state.pk.status === 'matching'"></MatchGround>
         <ResultBoard v-if="$store.state.pk.loser != 'none'"></ResultBoard>
+        <div class="user-color" v-if="$store.state.pk.status === 'playing' && parseInt($store.state.user.id) === parseInt($store.state.pk.a_id)">您位于左下角</div>
+        <div class="user-color" v-if="$store.state.pk.status === 'playing' && parseInt($store.state.user.id) === parseInt($store.state.pk.b_id)">您位于右上角</div>
     </div>
 </template>
     
@@ -92,5 +94,11 @@ export default{
 </script>
     
 <style scoped>
+div.user-color{
+    text-align: center;
+    color:aliceblue;
+    font-size: 30px;
+    font-weight: 600;
+}
     
 </style>
